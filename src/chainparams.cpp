33199f26f43ff28b732630a1491457433f0f0d23
RenosCoin <admin@renoscoin.com>
2017-05-29 10:04:52 -0600

    typos

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -94,8 +94,8 @@ public:
 		vSeeds.push_back(CDNSSeedData("seed1", "45.32.152.95"));
 		//vSeeds.push_back(CDNSSeedData("seed3", "45.76.42.66"));
 		//vSeeds.push_back(CDNSSeedData("seed4", "108.61.188.124"));
-        vSeed.push_back(CDNSSeedData("seed2", "45.76.37.221"));
-        vSeed.push_back(CDNSSeedData("seed3", "45.32.196.193");)
+        vSeeds.push_back(CDNSSeedData("seed2", "45.76.37.221"));
+        vSeeds.push_back(CDNSSeedData("seed3", "45.32.196.193"));
 
 		
         base58Prefixes[PUBKEY_ADDRESS] = list_of(60);
